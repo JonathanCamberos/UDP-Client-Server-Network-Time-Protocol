a2ff95938e8a3bd4076e68cd671135077827e246
JonathanCamberos <73202309+JonathanCamberos@users.noreply.github.com>
2022-10-22 18:33:06 -0400

    Update UDPClient.c

--- UDPClient.c
+++ UDPClient.c
@@ -512,7 +512,8 @@ void print_bytes(void *ptr, int size)
     unsigned char *p = ptr;
     int i;
     for (i=0; i<size; i++) {
-        printf("%02hhX ", p[i]);
+        //printf("%02hhX ", p[i]);
+		printf("\\%02hhX", p[i]);
     }
     //printf("\n");
 }
